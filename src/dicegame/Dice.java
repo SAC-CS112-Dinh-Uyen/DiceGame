2c455a6b2e6b05196c3305f7766e9576e24ce337
SAC-CS112-Dinh-Uyen <uyendinh171@gmail.com>
2016-11-08 17:02:59 -0800

    First commit

--- src/dicegame/Dice.java
+++ src/dicegame/Dice.java
@@ -1,36 +1,123 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dicegame;
-
-/**
- *
- * @author uyen
- */
+import java.util.Random;
+import java.util.Scanner;
 public class Dice {
-    private int numberOfRolls; // number of dices that user decides to roll.
-    private int dice1, dice2, dice3;
+    Scanner input = new Scanner (System.in);
+    private int numberOfDices; // number of dices that user decides to roll.
+    private int numberOfThrows; // number of times that user roll the dices
+    private int diceValue; // Current dice value
+    private int totalPoint=0; // total value when dices throw bounces times.
+    private int userPoint;//User guess points.
+    int i=0;//count to stop loop
+    private int average;
+    private int totalPlay=1;//Number of times user have played.
+    private int totalWin=0;//Number of times user win.
+    private float percentageWin;//winning percentage
+    
     //Constructor
-    public Dice(int num)
+    public Dice(int numDices, int numThrow)
     {
-        numberOfRolls = num;
+        numberOfDices=numDices;
+        numberOfThrows=numThrow;
     }
-    public void roll()
+    public Dice()
     {
-        dice1 = 1+(int)(Math.random()*6);
-        dice2 = 1+(int)(Math.random()*6);
-        dice3 = 1+(int)(Math.random()*6);
     }
-    //Get the total of point of dices
+    
+    // One dice with one throw
+    // return a random number between 1-6
+    public int OneDiceOneThrow()
+    {
+        diceValue = 1 + (int)(Math.random()*6);
+        return diceValue;
+    }
+    
+    // Multi dices with one throw
+    public int MultiDicesOneThrow()
+    {
+        for(int i=0; i<numberOfDices; i++)
+           totalPoint += OneDiceOneThrow();
+        return totalPoint;
+    }
+    
+    // Multi dices with multi throws
+    public void Throw()
+    {
+        for(int i=0; i<numberOfThrows;i++)
+            totalPoint += MultiDicesOneThrow();
+        average = totalPoint/numberOfThrows;
+    }
+    
     public int getTotalPoints()
     {
-        if(numberOfRolls==1)
-            return dice1;
-        else if(numberOfRolls==2)
-            return dice1+dice2;
-        else
-            return dice1+dice2+dice3;
+        return totalPoint;
+    }
+    
+    public int getAverage()
+    {
+        return average;
+    }
+    
+    public void playGame()
+    {
+        do{
+            // User chooses the number of dices to roll
+            System.out.println("How many dices would you like to roll?");
+            numberOfDices = input.nextInt();
+            // User chooses times to throw the dices
+            System.out.println("How many times would you like to throw the dices?");
+            numberOfThrows = input.nextInt();
+            
+            Dice myDice = new Dice(numberOfDices,numberOfThrows);
+            myDice.Throw();
+            totalPoint=myDice.getTotalPoints();
+            System.out.printf("Total points of %d dices with %d times throws are: %d\n",numberOfDices,numberOfThrows,totalPoint);
+            
+            //User guesses points
+            System.out.println("User guess points: ");
+            userPoint=input.nextInt();
+            
+            //Check whether user guesses right or wrong
+            if(userPoint==totalPoint){
+                totalWin++;
+                System.out.println("You win!");
+                System.out.printf("Total points of dices are: %d\n",totalPoint);
+            }
+            else{
+                System.out.println("You loose!");
+                System.out.printf("Total points of dices are: %d\n",totalPoint);
+            }
+            totalWin +=myDice.getTotalWin();
+            System.out.println("Would you like to play again? Y/N");
+            String answer=input.next();
+            String upperCaseAnswer=answer.toUpperCase();
+            if(upperCaseAnswer.equals("Y")){
+                i++;
+                totalPlay++;
+            }
+            else
+                i=0;
+                
+        }while(i!=0);
+    }
+    
+    public int getTotalWin()
+    {
+        return totalWin;
+    }
+    
+    public int getTotalPlay()
+    {
+        return totalPlay;
+    }
+    
+    public void history()
+    {
+        //Print out history game and winning percentage
+        percentageWin = (float)((getTotalWin()*100)/getTotalPlay());
+        System.out.printf("%nYou've played %d times.%n",getTotalPlay());
+        System.out.printf("You've won %d times.",getTotalWin());
+        System.out.printf("\nYour winning percentage is %s.%n",percentageWin+"%");
+        System.out.println("Goodbye!");
     }
 }
--- src/dicegame/DiceGame.java
+++ src/dicegame/DiceGame.java
@@ -5,54 +5,9 @@ import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        int numberOfRoll; //User decides number of dices to roll
-        int userPoint;//User guess points.
-        int dicePoint;//total of points of dices;
-        int i=0;//count to stop loop
-        int totalPlay=1;//Number of times user have played.
-        int totalWin=0;//Number of times user win.
-        float percentageWin;//winning percentage
-        //loop until user don't want to play again.
-        do{
-            System.out.println("Wellcome to Dice Game!(1 for One dice, 2 for Two dices and 3 for Three dices)");
-            // User chooses the number of roll
-            System.out.println("How many dices would you like to roll?");
-            numberOfRoll = input.nextInt();
-            Dice myDice = new Dice(numberOfRoll);
-            myDice.roll();
-            dicePoint=myDice.getTotalPoints();
-            System.out.printf("Total points of dices are: %d\n",dicePoint);
-            //User guesses points
-            System.out.println("User guess points: ");
-            userPoint=input.nextInt();
-            //Check if user guess right.         
-            if(userPoint==dicePoint){
-                totalWin++;
-                System.out.println("You win!");
-                System.out.printf("Total points of dices are: %d\n",dicePoint);
-            }
-            else{
-                System.out.println("You loose!");
-                System.out.printf("Total points of dices are: %d\n",dicePoint);
-            }
-            
-            System.out.println("Would you like to play again? Y/N");
-            String answer=input.next();
-            String upperCaseAnswer=answer.toUpperCase();
-            if(upperCaseAnswer.equals("Y")){
-                i++;
-                totalPlay++;
-            }
-            else
-                i=0;
-                
-        }while(i!=0);
-        //Print out history game and winning percentage
-        percentageWin = (float)((totalWin*100)/totalPlay);
-        System.out.printf("%nYou've played %d times.%n",totalPlay);
-        System.out.printf("You've won %d times.",totalWin);
-        System.out.printf("\nYour winning percentage is %s.%n",percentageWin+"%");
-        System.out.println("Goodbye!");
+        System.out.println("Wellcome to Dice Game!");
+        Dice myDice=new Dice();
+        myDice.playGame();
+        myDice.history();
     }
 }
